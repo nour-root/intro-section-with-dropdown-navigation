a4592f3ea92fcf74e808e415f84a04b166d1364b
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-03-12 19:27:06 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+let ShowModal = (id) => {
+  document.getElementById(id).style.transition = "all .3s";
+  document.getElementById(id).style.transform = "translateX(0)";
+  document.body.style.position = "fixed";
+  document.getElementById("overlay").classList.remove("hidden");
+};
+let HidModal = (id) => {
+  document.getElementById(id).style.transition = "all .3s";
+  document.getElementById(id).style.transform = "translateX(100%)";
+  document.body.style.position = "";
+  document.getElementById("overlay").classList.add("hidden");
+};
+
+let btnDrop = document.getElementById("btnDown");
+let drop1 = (id) => {
+  document.getElementById(id).classList.toggle("-rotate-180");
+  document.getElementById("list1").classList.toggle("hidden");
+  document.getElementById("list1_m").classList.toggle("hidden");
+  document.getElementById("btnDown2").classList.remove("-rotate-180");
+  document.getElementById("list2").classList.add("hidden");
+};
+let drop2 = (id) => {
+  document.getElementById(id).classList.toggle("-rotate-180");
+  document.getElementById("list2").classList.toggle("hidden");
+  document.getElementById("list2_m").classList.toggle("hidden");
+  document.getElementById("btnDown1").classList.remove("-rotate-180");
+  document.getElementById("list1").classList.add("hidden");
+};
+
+window.ShowModal = ShowModal;
+window.HidModal = HidModal;
+window.drop1 = drop1;
+window.drop2 = drop2;
